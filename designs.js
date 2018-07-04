e7c397dec36b758f9c98563f675468d6e0837375
lalitmee <lalitkumar.meena.lk@gmail.com>
2018-07-05 02:41:25 +0530

    final commit for the Pixel Art Maker

--- designs.js
+++ designs.js
@@ -1,36 +1,68 @@
 //variables to get canvas element,height,width
-const c = document.getElementById('pixel_canvas');
-let temph = $("#input_height");
-let tempw = $("#input_width");
+const c = document.getElementById("pixel_canvas");
+let heightIn = $("#height_input");
+let widthIn = $("#width_input");
 
 //An event listener that comes into action when Submit button is clicked .
-$('#input_submit').click(function(e) {
-    e.preventDefault();
-    makeGrid();
+$("#submit_input").click(function(e) {
+	e.preventDefault();
+	makeGrid();
 });
 
 let color = $("#colorPicker");
 
+$("#submit_input").on("click", function() {
+	var ele = document.getElementById("canavas");
+	ele.scrollIntoView();
+});
+
 //To make the grid pattern makegrid is called whenever button is clicked.
 function makeGrid() {
+	c.innerHTML = "";
+	let height = heightIn.val();
+	let width = widthIn.val();
 
-    c.innerHTML = '';
-    let height = temph.val();
-    let width = tempw.val();
+	//A function which fills color in the cell that was clicked and changes the color.
+	let addEvent = function(cell) {
+		cell.addEventListener("click", function() {
+			function setRgb() {
+				var red = document.querySelector(
+					".rgb-picker .red-slider input"
+				).value;
+				var green = document.querySelector(
+					".rgb-picker .green-slider input"
+				).value;
+				var blue = document.querySelector(
+					".rgb-picker .blue-slider input"
+				).value;
+				var color = "rgb(" + red + "," + green + "," + blue + ")";
+				document.querySelector(
+					".rgb-picker .color-preview"
+				).style.backgroundColor = color;
+			}
+			cell.style.backgroundColor = document.querySelector(
+				".rgb-picker .color-preview"
+			).style.backgroundColor;
+		});
+	};
 
-    //A function which fills color in the cell that was clicked and changes the color.
-    let addEvent = function(cell) {
-        cell.addEventListener('click', function() {
-            cell.style.backgroundColor = color.val();
-        });
-    }
-
-    //An event listener such that whenever any cell is clicked it calls addEvent function and changes it's color.
-    for (let i = 0; i < height; i++) {
-        let row = c.insertRow(i);
-        for (let j = 0; j < width; j++) {
-            let cell = row.insertCell(j);
-            cell.addEventListener('click', addEvent(cell));
-        }
-    }
+	//An event listener such that whenever any cell is clicked it calls addEvent function and changes it's color.
+	for (let i = 0; i < height; i++) {
+		let row = c.insertRow(i);
+		for (let j = 0; j < width; j++) {
+			let cell = row.insertCell(j);
+			cell.addEventListener("click", addEvent(cell));
+		}
+	}
 }
+
+function setRgb() {
+	var red = document.querySelector(".rgb-picker .red-slider input").value;
+	var green = document.querySelector(".rgb-picker .green-slider input").value;
+	var blue = document.querySelector(".rgb-picker .blue-slider input").value;
+	var color = "rgb(" + red + "," + green + "," + blue + ")";
+	document.querySelector(
+		".rgb-picker .color-preview"
+	).style.backgroundColor = color;
+}
+setRgb();
